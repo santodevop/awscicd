c56e856fe3d267ea916723b08eed5a915202af74
santodevop <130234487+santodevop@users.noreply.github.com>
2024-01-12 10:54:09 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Stop the running container (if any)
-containerid=$(sudo docker ps | awk 'NR==2{print $1}')
+containerid=$(sudo docker ps -q | head -n 1)
+
 if [ -n "$containerid" ]; then
   sudo docker rm -f "$containerid"
 fi
